7324e47cf75fe84172799d95a70f12419f08b2d1
lnagy2002 <kavoi2011@gmail.com>
2025-09-07 19:06:00 -0700

    Create shared.js

---
+++ docs/template/shared.js
@@ -0,0 +1,21 @@
+(async () => {
+  const headerHtml = await fetch('header.html').then(r => r.text());
+  const footerHtml = await fetch('footer.html').then(r => r.text());
+  document.getElementById('header').innerHTML = headerHtml;
+  document.getElementById('footer').innerHTML = footerHtml;
+  const y = document.getElementById('year');
+  if (y) y.textContent = new Date().getFullYear();
+})();
+
+async function loadData() {
+    try {
+        const res = await fetch('https://lnagy2002.github.io/factshistory/insurance/data/articles.json'); // <-- your JSON file
+        if (!res.ok) throw new Error(`HTTP ${res.status}`);
+        articles = await res.json();  
+        render();
+    } catch (err) {
+      console.error  (err);
+    }
+}
+
+loadData ();
