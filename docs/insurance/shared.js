8e31b8cf2502aa1dcd40abe9d98ac05fc375a96c
lnagy2002 <kavoi2011@gmail.com>
2025-09-07 17:58:23 -0700

    Update shared.js

--- docs/insurance/shared.js
+++ docs/insurance/shared.js
@@ -1,6 +1,6 @@
 (async () => {
-  const headerHtml = await fetch('partials/header.html').then(r => r.text());
-  const footerHtml = await fetch('partials/footer.html').then(r => r.text());
+  const headerHtml = await fetch('header.html').then(r => r.text());
+  const footerHtml = await fetch('footer.html').then(r => r.text());
   document.getElementById('header').innerHTML = headerHtml;
   document.getElementById('footer').innerHTML = footerHtml;
   const y = document.getElementById('year');
