ac97d060d4c1077fef8789fe32df9a5ee7a2dbee
lnagy2002 <kavoi2011@gmail.com>
2025-09-07 17:48:39 -0700

    Update shared.js

--- docs/insurance/shared.js
+++ docs/insurance/shared.js
@@ -6,3 +6,15 @@
   const y = document.getElementById('year');
   if (y) y.textContent = new Date().getFullYear();
 })();
+
+async function loadData() {
+    try {
+        const res = await fetch('https://lnagy2002.github.io/factshistory/insurance/data/articles.json"); // <-- your JSON file
+        if (!res.ok) throw new Error(`HTTP ${res.status}`);
+        ARTICLES = await res.json();   
+    } catch (err) {
+      console.error  (err);
+    }
+}
+
+loadData ();
